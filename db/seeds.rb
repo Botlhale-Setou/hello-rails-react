593e36782b3fa0db4b03fbf09991f6fc08e35f24
Botlhale Setou <botlhale.setou@gmail.com>
2023-05-11 22:48:19 +0200

    Create/Populate messages table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+messages = Message.create([
+  {message: 'Hello World!'},
+  {message: 'Bonjour Le Monde!'},
+  {message: 'Dumela Lefatshe!'},
+  {message: 'Salut Lume!'},
+  {message: 'Konnichiwa Sekai!'},
+])
