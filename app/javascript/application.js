7c70da4fb61ea5173ee8262af4237ae48ed33ac3
Botlhale Setou <botlhale.setou@gmail.com>
2023-05-12 00:27:11 +0200

    Complete React front-end

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,18 @@
 // Entry point for the build script in your package.json
+import '@hotwired/turbo-rails';
+import './controllers';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
